24d7547c0a8c0cafc0013534c78407c0b5566a1a
Smudge <nathan@ngriffith.com>
2012-11-17 21:08:48 -0500

    integrating all of the changes Adam made and cleaning them up a bit

--- public_folder/js/faceyourvote.js
+++ public_folder/js/faceyourvote.js
@@ -1,4 +1,8 @@
+var R;
 var currentState;
+
+var rStates = {};
+var cStates = {};
 
 var width = 800.0;
 var height = 500.0;
@@ -38,7 +42,7 @@ function totalEvBoxes() {
 }
 
 var selectState = function(st) {
-  st.animate({"stroke-width": "3",stroke: "#fff"}, 75);
+  st.animate({"stroke-width": "3",stroke: "#af4"}, 75); // highlight
   st.toFront();
 };
 
@@ -83,17 +87,48 @@ function cInWords(c) {
   return words;
 };
 
+function cInColors(c) {
+  var Colors;
+  switch(c)
+  {
+  case 0:
+    Colors = "#18286b";
+    break;
+  case 1:
+    Colors = "#5b6795";
+    break;
+  case 2:
+    Colors = "#8a91b1";
+    break;
+  case 3:
+    Colors = "#fff";
+    break;
+  case 4:
+    Colors = "#c28280";
+    break;
+  case 5:
+    Colors = "#aa5252";
+    break;
+  case 6:
+    Colors = "#880a0a";
+    break;
+  default:
+    Colors = "#fff";
+  }
+  return Colors;
+};
+
 function pulse(obj,margin){
   if (!margin) { margin='marginLeft'; }
   obj.stop();
   obj.stop();
   var animStart = {
-    width: '106px',
-    height: '106px',
-    backgroundSize: '106px',
-    marginTop: '-3px'
+    width: '110px',
+    height: '110px',
+    backgroundSize: '112px',
+    marginTop: '-5px'
   };
-  animStart[margin] = '-3px';
+  animStart[margin] = '-5px';
   var animEnd = {
     width: "100px",
     height: "100px",
@@ -102,8 +137,8 @@ function pulse(obj,margin){
   };
   animEnd[margin] = "0px";
   obj.animate(animEnd,0);
-  obj.animate(animStart,100);
-  obj.animate(animEnd,100);
+  obj.animate(animStart,0);
+  obj.animate(animEnd,200);
 };
 
 function initializeSlider() {
@@ -112,25 +147,48 @@ function initializeSlider() {
   for (var c in summary) {
     total+= summary[c];
   }
+
   for (var c in summary) {
     $("#c"+c).width((summary[c]*100/total)+"%");
   }
+  
+  return(total);
 }
 
 function initializeLastUpdated() {
   var time = fyv_data["source"]["timestamp"];
-  var datetime = Date.parse(time);
-  $("#lastUpdated").html("Last Updated: "+datetime);
+//  var datetime = Date.parse(time);
+  var year = time.substring(0,4)
+  var month = time.substring(4,6)  
+  var day = time.substring(6,8)    
+  var MyUpdate = parseDate(year + '-' + month + '-' + day);
+  $("#lastUpdated").html("Last Updated: "+ MyUpdate);
 }
 
-var rStates = {};
+// parse a date in yyyy-mm-dd format
+function parseDate(input) {
+  var parts = input.match(/(\d+)/g);
+  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+  var myString = new Date(parts[0], parts[1]-1, parts[2])
+  var txt = new String(myString);
+  var mySplitResult = txt.split(" ");
+	var MyFormatted = mySplitResult[0] + ' ' + mySplitResult[1] + ' ' + mySplitResult[2] + ' ' + mySplitResult[3];	
+  return MyFormatted; // months are 0-based
+}
+
+function changeFaces(c) {
+  $("#romney").css('background-image', "url(img/r"+c+".jpg)");
+  $("#obama").css('background-image', "url(img/d"+(6-c)+".jpg)");
+  pulse($("#obama"));
+  pulse($("#romney"),'marginRight');
+}
 
 window.onload = function () {
-  var R = Raphael("container", width, height),
+  R = Raphael("container", width, height),
     attr = defaultAttr
 
   totalEvBoxes();
-  initializeSlider();
+  MyTotal = initializeSlider();
   initializeLastUpdated();
 
   //Draw Map and store Raphael paths
@@ -146,14 +204,16 @@ window.onload = function () {
       // var rep = (stateData[fullStateName][2]-4)/2.0;
       var dem = fyv_data["states"][stateName]["d"]/100.0;
       var rep = fyv_data["states"][stateName]["r"]/100.0;
+		  var c = fyv_data["states"][stateName]["c"];
+		
       //alert("dem: "+dem+"| rep: "+rep);
 
-      if (rep > dem) {
+      if (c > 3) {
         rState.color = "rgba("+repR+","+repG+","+repB+","+(rep-0.5)*2+")";
-      } else if (rep < dem) {
+      } else if (c < 3) {
         rState.color = "rgba("+demR+","+demG+","+demB+","+(dem-0.5)*2+")";
       } else {
-        rState.color = "rgba(150,100,150,0.5)";
+        rState.color = "rgba(255,255,255,0.8)";
       }
 
       rState.attr({fill: rState.color});
@@ -164,45 +224,38 @@ window.onload = function () {
         var star = "<span>&#9733;</span>";
         var fullStateName = fyv_data["states"][stateAbbrev]["n"];
         var c = fyv_data["states"][stateAbbrev]["c"];
+        
+        if (cStates[c] == null) {
+          cStates[c] = [];
+        }
+        cStates[c].push(rStates[stateName]);
+        
         var dem = 3 - c;
         var rep = c - 3;
 
         $(st[0]).hover(function () {
           if (currentState != st) {
+      			var bar = (fyv_data["states"][stateAbbrev]["e"]*100.00/MyTotal)*10;
+      			var fixedbar = bar.toFixed(0);
+      			var MyLeft = (750 - fixedbar)/2;
+      			MyLeft.toFixed(0);			
+      			var MyColor = cInColors(c);
+      			$("#MyBar").css({'display':'block', 'width' : fixedbar, 'margin-left' : MyLeft, 'background-color' : MyColor});
+								
             selectState(st);
             cleanup(R,rStates);
-            $("#hoverTip").fadeOut();
-            $("#mainHeader").html(star+fullStateName+star);
-            $("header h3").html(cInWords(c));
-            $("#romney").css('background-image', "url(/img/r"+c+".jpg)");
-            $("#obama").css('background-image', "url(/img/d"+(6-c)+".jpg)");
-            pulse($("#obama"));
-            pulse($("#romney"),'marginRight');
+            changeFaces(c);
             currentState = st;
-            evs = fyv_data["states"][stateAbbrev]["e"];
-            if (evs) {
-              if (dem > rep) {
-                setEvBoxes(evs,0);
-              }
-              if (rep > dem) {
-                setEvBoxes(0,evs);
-              }
-              if (rep === dem) {
-                setEvBoxes(evs+"?",evs+"?");
-              }
-            }
-          }
+            
+	        }
         }, function () {
           deselectState(st);
           cleanup(R,rStates);
-          $("#mainHeader").html("Face"+star+"Your"+star+"Vote");
-          $("header h3").html("getting straight to the point");
-          $("#romney").css('background-image', "url(/img/r2.jpg)");
-          $("#obama").css('background-image', "url(/img/d2.jpg)");
+          $("#romney").css('background-image', "url(img/r2.jpg)");
+          $("#obama").css('background-image', "url(img/d2.jpg)");
+          $("#MyBar").css('display', "none");
           currentState = null;
           totalEvBoxes();
-        }).click(function () {
-          //Maybe sticky-select on click?
         }).css('cursor', 'pointer');
 
       })(rStates[stateName], stateName);
@@ -210,3 +263,40 @@ window.onload = function () {
   }
   cleanup(R,rStates);
 };
+
+$(document).ready(function() {
+  $( "#open-event" ).tooltip({
+    show: null,
+    position: {
+      my: "center top",
+      at: "center bottom"
+    }
+  });
+  $('.slider').hover( function(){
+      BarStates($(this).data("num"));
+   },
+   function(){
+      HideStates($(this).data("num"));
+   });
+});
+
+function HideStates(cValue){
+  var theStates = cStates[cValue];
+  for (var i = 0; i < theStates.length; i++) {
+      //console.log(theStates[i]);
+      deselectState(theStates[i]);
+  }
+  $("#romney").css('background-image', "url(img/r2.jpg)");
+  $("#obama").css('background-image', "url(img/d2.jpg)");
+  cleanup(R,rStates);
+}
+
+function BarStates(cValue) {
+  var theStates = cStates[cValue];
+  for (var i = 0; i < theStates.length; i++) {
+      //console.log(theStates[i]);
+      selectState(theStates[i]);
+  }
+  changeFaces(cValue);
+  cleanup(R,rStates);
+}
